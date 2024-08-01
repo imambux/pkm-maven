f90af51491dec63682f56f83735d6d3024d38901
imambux <imam.bux@scout24.com>
2024-08-01 15:07:25 +0200

    A demo test

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,19 @@
+public class HelloWorldTest {
+
+    public void test1() {
+        System.out.println("Hello World test 1");
+    }
+
+    public void test2() {
+        System.out.println("Hello World test 2");
+    }
+
+    public void test3() {
+        System.out.println("Hello World test 3");
+    }
+
+    public void noTest4() {
+        System.out.println("This will not run because the method is not prefixed with test.");
+    }
+
+}
