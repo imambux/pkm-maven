71ef97f17eeeb231e5266251259e25cc71a502f2
imambux <imam.bux@scout24.com>
2024-08-02 14:30:28 +0200

    Setup Integration Test

---
+++ src/test/java/com/example/util/HelloWorldIT.java
@@ -0,0 +1,14 @@
+package com.example.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldIT {
+
+    @Test
+    void demoIntegrationTest() {
+        assertEquals(true, true);
+    }
+
+}
