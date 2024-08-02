0c1950a9364a9530f817d9194fd64ceca6bec664
imambux <imam.bux@scout24.com>
2024-08-02 11:56:08 +0200

    Write POJO test

---
+++ src/main/java/com/example/util/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.util;
+
+public class HelloWorld {
+
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
+
+}
---
+++ src/test/java/com/example/util/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.example.util;
+
+
+public class HelloWorldTest {
+
+    public void testHelloWorld() {
+        assert("Hello World!".equals(new HelloWorld().getHelloWorld()));
+    }
+
+}
